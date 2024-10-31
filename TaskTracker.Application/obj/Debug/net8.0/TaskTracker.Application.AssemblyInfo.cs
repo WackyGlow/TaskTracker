3f8ceeff837520c1488c6fae3dccab3ffbd50fab
Rasmus Thyregod Kristensen <stampe12@gmail.com>
2024-10-31 14:44:30 +0100

    refactored service interfaces

--- TaskTracker.Application/obj/Debug/net8.0/TaskTracker.Application.AssemblyInfo.cs
+++ TaskTracker.Application/obj/Debug/net8.0/TaskTracker.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+237e07c067795cce1fefba7576b359782e15420a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
